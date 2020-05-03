b8b9e15f348678c5dd22e1afb85f0820910c33ea
lucas <lucas@anezin.com>
2020-05-03 19:46:40 -0400

    Primer commit

---
+++ app/src/main/java/com/anezin/panaderiaedfamilia/ui/home/HomeFragment.java
@@ -0,0 +1,115 @@
+package com.anezin.panaderiaedfamilia.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.anezin.panaderiaedfamilia.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+
+public class HomeFragment extends Fragment {
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("Usuarios");
+
+    TextView mConditionTextView;
+    TextView estadoFioreTw;
+
+    RecyclerView recyclerView;
+    ArrayList<String> listDatos = new ArrayList<String>();
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        mConditionTextView = root.findViewById(R.id.text_home);
+        estadoFioreTw = root.findViewById(R.id.fiore);
+
+        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerView1);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setLayoutManager(new GridLayoutManager(root.getContext(), 2));
+
+        refrescarLista();
+
+
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+              //   collectEstados((Map<String,Object>) dataSnapshot.getValue());
+
+                listDatos.clear();
+
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+
+                    listDatos.add(child.getKey());
+
+                    for (DataSnapshot child2 : child.getChildren()) {
+                        listDatos.add(child2.getValue().toString());
+                    }
+
+
+                }
+                refrescarLista();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        return root;
+    }
+
+    private void collectEstados(Map<String,Object> usuarios) {
+
+        ArrayList<Long> estados = new ArrayList<>();
+
+        for (Map.Entry<String, Object> entry : usuarios.entrySet()){
+
+            Map singleUser = (Map) entry.getValue();
+
+            estados.add((Long) singleUser.get("Pagado"));
+        }
+
+        System.out.println(estados);
+
+    }
+
+    private void refrescarLista(){
+        AdapterDatos adapter = new AdapterDatos(listDatos);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+
+}
